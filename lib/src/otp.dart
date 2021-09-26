74378766a819405994d53bde9e65087525a3e6c4
Ernesto Ittig <e.ittig@gmail.com>
2021-09-26 16:00:38 -0300

    made generation function more memory efficient

--- lib/src/otp.dart
+++ lib/src/otp.dart
@@ -5,9 +5,9 @@ import 'dart:typed_data';
 /// Generate a new one-time pad of [length]
 Uint8List generateOneTimePad([int length = 100]) {
   var random = Random.secure();
-  var returnValue = <int>[];
+  var returnValue = Uint8List(length);
   for (var i = 0; i < length; ++i) {
-    returnValue.add(random.nextInt(26));
+    returnValue[i] = random.nextInt(26);
   }
-  return Uint8List.fromList(returnValue);
+  return returnValue;
 }
